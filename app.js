e7cbd042f0d1cac3baa3cbf2c95141d8a1929e50
Prathamesh More <moreprathameshmore@hotmail.com>
2019-09-21 20:42:44 +0530

    Add random route

--- app.js
+++ app.js
@@ -9,47 +9,54 @@ const Quote = require("./models/quote");
 const PORT = 3000;
 const URL = "192.168.43.61";
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 //Database connection
 mongoose.connect("mongodb://localhost:27017/quotegarden", { useNewUrlParser: true }, (error) => {
     if (error) {
-        console.log("Error");
+        console.log("Ooops!!! Something went wrong! :(");
     } else {
-        console.log("Successfully connected to database");
+        console.log("Successfully connected to database :)");
     }
 });
 
 //Simple GET method
 app.get("/", (request, response) => {
-    response.send("Hello from API");
-})
+    //response.send("Hello from API!!! I am running properly! :)");
+    
+    response.status(200).render("index");
+});
 
 //Simple random requests
-app.get("/random", async (request, response) => {
-
-    const documentCount = await Quote.estimatedDocumentCount();
-    const index = random
-
-    Quote.find({}, (error, quote) => {
-        if (error) {
-            console.log("Error");
-        } else {
-            response.json(quote);
-        }
-    })
-})
+app.get("/quotes/random", async (request, response) => {
+    Quote.count().exec(function (error, count) {
+        var random = Math.floor(Math.random() * count);
+        Quote.findOne().skip(random).exec(function (error, quote) {
+            response.status(200).json(quote);
+        });
+    });
+});
 
 //Routes to find as per search
-app.get("/:author", (request, response) => {
-    response.send("All Authors");
+app.get("quotes/", (request, response) => {
+    // response.send("All Authors");
 });
 
 //Get all quotes
-app.get("/all", (request, response) => {
+app.get("quotes/all", (request, response) => {
     response.send("All Quotes");
-})
+});
 
+app.get("quotes/new", (request, response) => {
+    response.status(200).render("index");
+});
+
+app.get("*", (request, response) => {
+    response.status(404).render("error");
+});
 
 //Start server
 app.listen(PORT, () => {
     console.log("Server running on port : " + PORT)
-})
+});

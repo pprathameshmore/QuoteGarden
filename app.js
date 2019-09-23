a385e0db936a743a5eebe5393ea4df02508e5d04
Prathamesh More <moreprathameshmore@hotmail.com>
2019-09-23 20:31:22 +0530

    Minnor changes

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const Quote = require("./models/quote");
 //const PORT = 3000;
 //const URL = "192.168.43.61";
 const PORT = process.env.PORT || 3000
-`
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));

119e80327debef3c6669c47a641bc584aba305cf
Prathamesh More <moreprathameshmore@hotmail.com>
2019-10-11 15:53:48 +0530

    Fix Access Control Allow Origin

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
 
 //Database connection
 mongoose.connect(DB_URL, {

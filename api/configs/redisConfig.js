cd6d8f6f84c8fde1e95677d7436eb603e13368ef
Prathamesh More <moreprathameshmore@hotmail.com>
2020-04-06 20:10:42 +0530

    Fix link

--- api/configs/redisConfig.js
+++ api/configs/redisConfig.js
@@ -1,11 +1,11 @@
 const redis = require('redis');
 
 
-const redisPort = process.env.PORT || 6379;
+//const redisPort = process.env.PORT || 6379;
 /*
 Redis connection
 */
-const redisClient = redis.createClient(process.env.REDIS);
+const redisClient = redis.createClient('redis://redistogo:818dc14eb0456fc5bfec46df3e64edd2@pike.redistogo.com:10617/');
 redisClient.on('error', (err) => {
     console.log(err);
 });

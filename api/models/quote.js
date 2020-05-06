f20709ee452349e49404d3613b860a2f07ade22b
Prathamesh More <moreprathameshmore@hotmail.com>
2020-05-06 12:17:27 +0530

    Temp fix

--- api/models/quote.js
+++ api/models/quote.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 //Define schema for quotes
 const quoteSchema = new mongoose.Schema({
     quoteText: String,
-    quoteAuthor: String
+    quoteAuthor: String,
+    quoteGenre: String
 });
 
 //Create model on the basis of quote schema
